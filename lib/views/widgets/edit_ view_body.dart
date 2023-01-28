5fdcdd6d92dd8ba1e658ce5801c0581f378250e3
Norhan-127 <norhanhamza127@gmail.com>
2023-01-29 01:19:02 +0200

    edit note view ui done

--- lib/views/widgets/edit_ view_body.dart
+++ lib/views/widgets/edit_ view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/CustomAppbar.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({Key? key}) : super(key: key);
@@ -12,6 +13,10 @@ class EditNoteViewBody extends StatelessWidget {
       child: Column(
         children: const [
           CustomAppBar(title: 'Edit',icon: CupertinoIcons.checkmark_alt,),
+          SizedBox(height: 32,),
+          CustomTextField(hint: 'Title'),
+          SizedBox(height: 16,),
+          CustomTextField(hint: 'Content',maxLines: 5,),
         ],
       ),
     );
