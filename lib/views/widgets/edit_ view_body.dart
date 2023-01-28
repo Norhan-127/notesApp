02626c7569aacd60070d134e91ac9d2c8abb5419
Norhan-127 <norhanhamza127@gmail.com>
2023-01-29 01:41:47 +0200

    create note model

--- lib/views/widgets/edit_ view_body.dart
+++ lib/views/widgets/edit_ view_body.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/CustomAppbar.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
