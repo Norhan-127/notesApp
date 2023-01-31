518a19fc64c2437e26ed56052c18fe3c05c7d4ad
Norhan-127 <norhanhamza127@gmail.com>
2023-01-31 18:35:51 +0200

    add addNote() method to AddNoteProvider class

--- lib/controller/add_note_model/add_note_prov.dart
+++ lib/controller/add_note_model/add_note_prov.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/cupertino.dart';
-class AddNote with ChangeNotifier{
-  addNote(AddNote note){
-
+import 'package:hive/hive.dart';
+import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/utilities/constants.dart';
+class AddNoteProvider with ChangeNotifier{
+  bool addNoteLoading=false;
+  addNote(NoteModel note) async{
+    try {
+      addNoteLoading = true;
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      await notesBox.add(note);
+      debugPrint('Note has been added successfully!');
+      addNoteLoading = false;
+      notifyListeners();
+    }catch(e){
+      addNoteLoading=false;
+      debugPrint(e.toString());
+    }
   }
 }
